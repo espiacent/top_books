e391926a03a7cccc44a99e75785f14435c83c17c
espiacent <ch.gripp@posteo.ch>
2022-09-13 01:01:55 +0200

    add basic functionality (create book object and add object to list)

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const booklist = [];
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
@@ -9,8 +10,8 @@ Book.prototype.getInfo = function () {
     return this.title + ' by ' + this.author + ', ' + this.pages + ' pages, reading status: ' + this.read + '.'
 };
 
-const Thud = new Book('Thud', 'Terry Pratchett', '312', 'currently reading');
-const GoingPostal = new Book('Going Postal', 'Terry Pratchett', '410', 'read');
+const Book1 = new Book('Thud', 'Terry Pratchett', '312', 'currently reading');
+const Book2 = new Book('Going Postal', 'Terry Pratchett', '410', 'read');
 
 const addbtn = document.querySelector('.book-add');
 const closebtn = document.querySelector('.modal-close-btn');
@@ -33,5 +34,16 @@ addbtn.addEventListener('mouseup', function () {
 });
 
 closebtn.addEventListener('mouseup', function () {
+    popup.classList.toggle('open');
+});
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let title = form.elements['title'].value;
+    let author = form.elements['author'].value;
+    let pages = form.elements['pages'].value;
+    let status = form.elements['status'].value;
+    booklist.push(new Book(title, author, pages, status));
+    document.getElementById("form").reset();
     popup.classList.toggle('open');
 });
